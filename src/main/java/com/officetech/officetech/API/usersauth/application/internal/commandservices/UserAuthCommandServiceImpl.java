82de6a98dd4fec6c4d1ff71e9129c604fcb5e1cf
Arian Rodriguez <arianmrv12@gmail.com>
2024-05-27 00:17:11 -0500

    add: command service to save new user

---
+++ src/main/java/com/officetech/officetech/API/usersauth/application/internal/commandservices/UserAuthCommandServiceImpl.java
@@ -0,0 +1,32 @@
+package com.officetech.officetech.API.usersauth.application.internal.commandservices;
+
+import com.officetech.officetech.API.usersauth.domain.model.aggregates.UserAuth;
+import com.officetech.officetech.API.usersauth.domain.model.commands.CreateUserAuthCommand;
+import com.officetech.officetech.API.usersauth.domain.services.UserAuthCommandService;
+import com.officetech.officetech.API.usersauth.infrastructure.persistance.jpa.UserAuthRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserAuthCommandServiceImpl implements UserAuthCommandService {
+    private final UserAuthRepository userAuthRepository;
+
+    public UserAuthCommandServiceImpl(UserAuthRepository userAuthRepository) {
+        this.userAuthRepository = userAuthRepository;
+    }
+
+    @Override
+    public Optional<UserAuth> handle(CreateUserAuthCommand command) {
+        System.out.println("UserAuthCommandServiceImpl: Handling create user command");
+        /*
+        if (userAuthRepository.existsById(command.id())) {
+            throw new IllegalArgumentException("User with same user ID already exists");
+        }*/
+        var userAuth = new UserAuth(command);
+        System.out.println("UserAuthCommandServiceImpl: Saving user entity");
+        var createdUserAuth = userAuthRepository.save(userAuth);
+        System.out.println("UserAuthCommandServiceImpl - Repository: User entity saved");
+        return Optional.of(createdUserAuth);
+    }
+}
