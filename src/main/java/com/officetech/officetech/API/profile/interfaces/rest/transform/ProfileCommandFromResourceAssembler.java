19a7e92e28f6c87e3621ed89c1d62c458ba4880f
QuiqueVladimir <U202022365@upc.edu.pe>
2024-06-07 17:57:54 -0500

    feat: added transform ProfileCommandFromResourceAssembler

---
+++ src/main/java/com/officetech/officetech/API/profile/interfaces/rest/transform/ProfileCommandFromResourceAssembler.java
@@ -0,0 +1,14 @@
+package com.officetech.officetech.API.profile.interfaces.rest.transform;
+
+import com.officetech.officetech.API.profile.domain.model.commands.UpdateProfileCommand;
+import com.officetech.officetech.API.profile.interfaces.rest.resources.ProfileResource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProfileCommandFromResourceAssembler {
+    public static UpdateProfileCommand toCommandFromResource(ProfileResource resource) {
+        System.out.println("ProfileCommandFromResourceAssembler: converting resource to command");
+        return new UpdateProfileCommand(resource.id(), resource.firstName(), resource.lastName(), resource.email(), resource.phone(), resource.password());
+    }
+}
+
